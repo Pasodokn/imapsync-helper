5c541b322b90d7dcbce9a874c35ee4e8201cd677
root <root@turing.pasodokn.dev>
2020-12-04 23:38:16 +0000

    Updated

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en" id="doc">
+  <head>
+    <header style="float: right;">
+      <a class="link" href="https://github.com/Pasodokn/gst-imapsync" target="_blank">gst-imapsync</a> || 
+      <a class="link" href="https://github.com/Pasodokn/" target="_blank">Pasodokn</a>
+    </header>
+    <title>IMAPSync Helper</title>
+    <meta name="description" content="A tool to generate the command for an IMAP sync">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script src="public/js/index.js"></script>
+    <script src="https://code.jquery.com/jquery-latest.js"></script>
+  </head>
+  <body>
+    <h1>Email Migration Tool</h1>
+    <main id="main">
+      <div>
+        <form style="width: 100%; overflow: hidden; padding: 12px;">
+          <div class="custombg" style="width: 45%; float: left; padding: 12px;">
+            <h3>Source</h3>
+            <h2>Server / Hostname:</h2>
+            <input class="custom" name="host1" id="host1" type="text" maxlength="100" placeholder="Host 1">
+            <h2>Email Address:</h2>
+            <input class="custom" name="user1" id="user1" type="email" maxlength="75" placeholder="User 1" onchange="updateCPUser();">
+            <h2>Password:</h2>
+            <input class="custom" name="pass1" id="pass1" type="text" maxlength="50" placeholder="Pass 1" onchange="updateCPPass();">
+            <div>
+              <br>Port
+              <input class="custom" name="port1" id="port1" type="number" maxlength="5" placeholder="Port 1" style="width: 65px;">
+              <br>SSL
+              <input id=ssl1 name="ssl1" type="checkbox" style="width: unset;">
+            </div>
+            <h2 class="bold">Presets:</h2>
+            <select id="type1" onchange="setType1();">
+              <option>Other</option>
+              <option>Google / GSuite</option>
+              <option>Office 365</option>
+            </select>
+          </div>
+          <div class="custombg" style="margin-left: 50%; width: 45%; padding: 12px;">
+            <h3>Destination</h3>
+            <h2>Server / Hostname:</h2>
+            <input class="custom" name="host2" id="host2" type="text" maxlength="100" placeholder="Host 2">
+            <h2>Email Address:</h2>
+            <input class="custom" name="user2" id="user2" type="email" maxlength="75" placeholder="User 2">
+            <input type="checkbox" name="cpuser" id="cpuser">
+            <h2>Password:</h2>
+            <input class="custom" name="pass2" id="pass2" type="text" maxlength="50" placeholder="Pass 2">
+            <input type="checkbox" name="cppass" id="cppass">
+            <div>
+              <br>Port
+              <input class="custom" name="port2" id="port2" type="number" maxlength="5" placeholder="Port 2" style="width: 65px;">
+              <br>SSL
+              <input id=ssl2 name="ssl2" type="checkbox" style="width: unset;">
+              <br><br><br>
+            </div>
+          </div>
+        </form>
+      </div>
+    <div style="text-align: center; padding-top: 20px;">
+      <button id="submit" onclick="updateCommand();" style="background-color: green;">Update</button>
+      <input class="custom" id="result" type="text" style="width: 70%;">
+      <button id="copy">Copy text</button>
+      <input class="cbutton" type="submit" value="Run Test" onclick="syncRequest(document.getElementById('result').value);" id="runtest" style="background-color: red;">
+    </div>
+    <div style="width: 30%; float: left; position: absolute;">
+      <br><br><br><br>
+      <table>
+        <tr>
+          <th class="tableHead">Recent Tests</th>
+        </tr>
+        <tr>
+          <th>$logEmailAcc1</th>
+          <th>$logEmailResults1</th>
+        </tr>
+        <tr>
+          <th>$logEmailAcc2</th>
+          <th>$logEmailResults2</th>
+        </tr>
+        <tr>
+          <th>$logEmailAcc3</th>
+          <th>$logEmailResults3</th>
+        </tr>
+      </table>
+    </div>
+    <div id="tdetails" style="width: 60%; float: right;">
+      <br><br><br>
+      <table>
+        <tr>
+          <th class="tableHead">Test Details</th>
+        </tr>
+        <tr>
+          <th>IMAPSYNC:</th>
+          <th>$accResult</th>
+        </tr>
+        <tr>
+          <th>Account Size: </th>
+          <th>$accDisk</th>
+        </tr>
+        <tr>
+          <th>Emails: </th>
+          <th>$accEmails</th>
+        </tr>
+        <tr>
+          <th>Estimated Time: </th>
+          <th>$accETA</th>
+        </tr>
+        <tr>
+          <th>Date/Time: </th>
+          <th id="logtime"></th>
+        </tr>
+        <tr>
+          <th id="log">PLACEHOLDER</th>
+        </tr>
+      </table>
+    </div>
+    <script>
+      $("#copy").on('click', function() {
+          copytext = document.getElementById("result");
+          copytext.select();
+          document.execCommand("copy");
+        });
+    </script>
+    <script>
+      $(function() {
+        $("#runtest").hide();
+        $("#tdetails").hide();
+        $("#runtest").on('click', function() {
+          $("#tdetails").show(400);
+        });
+      });
+    </script>
+    </main>
+  </body>
+</html>
---
+++ public/js/index.js
@@ -0,0 +1,109 @@
+function setType1() {
+  let e = document.getElementById('type1').options[document.getElementById('type1').selectedIndex].text;
+
+  if (e == "Google / GSuite") {
+    document.getElementById("host1").value = "imap.gmail.com";
+    document.getElementById("port1").value = "993";
+    document.getElementById("ssl1").value.checked = true;
+  } else
+  if (e == "Other") {
+    document.getElementById("host1").value = "";
+    document.getElementById("port1").value = "993";
+    document.getElementById("ssl1").checked = true;
+  } else
+  if (e == "Office 365") {
+    document.getElementById("host1").value = "outlook.office365.com";
+    document.getElementById("port1").value = "993";
+    document.getElementById("ssl1").checked = true;
+  }
+}
+
+function updateCPUser() {
+  if (document.getElementById("cpuser").checked === true)
+    document.getElementById("user2").value = document.getElementById("user1").value;
+};
+
+function updateCPPass() {
+  if (document.getElementById("cppass").checked === true)
+    document.getElementById("pass2").value = document.getElementById("pass1").value;
+};
+
+function updateCommand() {
+  //Reset the result box
+  let command = "";
+
+  let host1 = document.getElementById('host1').value;
+  let user1 = document.getElementById('user1').value;
+  let pass1 = document.getElementById('pass1').value;
+  let port1 = document.getElementById('port1').value;
+  let host2 = document.getElementById('host2').value;
+  let user2 =document.getElementById('user2').value;
+  let pass2 = document.getElementById('pass2').value;
+  let port2 = document.getElementById('port2').value;
+  let ssl1 = document.getElementById('ssl1').checked;
+  let ssl2 = document.getElementById('ssl2').checked;
+
+  let fields ={
+    host1: host1, 
+    user1: user1, 
+    port1: port1, 
+    host2: host2, 
+    user2: user2, 
+    port2: port2
+  };
+
+  Object.keys(fields).forEach(function(key) {
+    if (fields[key] == "")
+      console.log("Field \'" + key + "\' is empty.")
+    else
+      command += " --" + key + " " + fields[key]; 
+  })
+
+  if (pass1 == "") {
+    console.log("Field \'password1\' is empty.")
+    _pass1 = "";
+  } else _pass1 = " --password1 " + pass1;
+
+  if (pass2 == "") {
+    console.log("Field \'password2\' is empty.")
+    _pass2 = "";
+  } else _pass2 = " --password2 " + pass2;
+
+  if (ssl1 == true)
+    _ssl1 = " --ssl1";
+  else _ssl1 = "";
+  
+  if (ssl2 == true)
+    _ssl2 = " --ssl2";
+  else _ssl2 = "";
+
+  command += _pass1 + _pass2 + _ssl1 + _ssl2;
+
+  updateResult(command);
+};
+
+function updateResult(command) {
+  document.getElementById('result').value = "imapsync" + command;
+  $(function() {
+    $("#runtest").show(500);
+  });
+};
+
+function syncRequest(str) {
+  let response;
+  if (str.length == 0) {
+      document.getElementById("log").innerHTML = "";
+      return;
+  } else {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              response = this.responseText.split("...");
+              document.getElementById("logtime").innerHTML = response[0];
+              document.getElementById("log").innerHTML = response[1];
+          }
+      };
+      xmlhttp.open("GET", "sync.php?request=" + str, true);
+      xmlhttp.send();
+  }
+}
